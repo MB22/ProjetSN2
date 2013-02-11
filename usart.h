96985fe8898151dd73fd361de6cf3e8a56f16e8a
MB22 <matthieu.biache@gmail.com>
2013-02-11 17:40:28 +0100

    Update usart.h

--- usart.h
+++ usart.h
@@ -1,19 +1,19 @@
 /*
-usart.h
+usart1.h
 Crée par Matthieu Biache et Quentin Viotto le 11 février 2013
 Derniere modification: Biache Matthieu 11 février 2013
 Fichier de descritpion usart
 */
 
-#ifndef __USART_H
-#define __USART_H
+#ifndef __USART1_H
+#define __USART1_H
 
 #include <91M55800.h>
 
 #include "global.h"
 #include "constante.h"
 
-void fct_init_USART(void); //Fonction d'initialisation de l'USART
-void fct_start_USART(void); //Fonction de démarrage de l'USART
+void fct_init_USART1(void); //Fonction d'initialisation de l'USART
+void fct_start_USART1(void); //Fonction de démarrage de l'USART
 
 #endif
